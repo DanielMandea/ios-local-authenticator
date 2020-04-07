5dd15f385ad56ef40ee67e051cc56aa6cc6b8e52
Daniel Mandea <daniel.mandea@yahoo.com>
2020-04-08 00:08:30 +0300

    Fixed package dependencies

--- Package.swift
+++ Package.swift
@@ -21,15 +21,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../Keychain"),
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.13.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Authenticator",
-            dependencies: ["Keychain", "PromiseKit"]),
+            dependencies: []),
         .testTarget(
             name: "AuthenticatorTests",
             dependencies: ["Authenticator"]),
